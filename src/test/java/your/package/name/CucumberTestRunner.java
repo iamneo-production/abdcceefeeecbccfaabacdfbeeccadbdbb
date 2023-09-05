285ce09f3b3bcd5afe057f827ac31b104bf57b0e
Iamneo <techadmin@iamneo.ai>
2023-09-05 14:08:57 +0000

    Backup Commit

--- src/test/java/your/package/name/CucumberTestRunner.java
+++ src/test/java/your/package/name/CucumberTestRunner.java
@@ -1,5 +1,12 @@
-package your.package.name;
+import cucumber.api.CucumberOptions;
+import cucumber.api.junit.Cucumber;
+import org.junit.runner.RunWith;
 
+@RunWith(Cucumber.class)
+@CucumberOptions(
+    features = "src/test/resources/features",
+    glue = "your.package.name", // Replace with the package where your step definitions are located
+    plugin = {"pretty", "html:target/cucumber-reports"}
+)
 public class CucumberTestRunner {
-    
 }
